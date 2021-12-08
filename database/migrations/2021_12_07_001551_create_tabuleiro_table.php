b4e602d90a4c7e4c19d541a763060e3765824d73
Vinicius Pires <viniciusapires@yahoo.com.br>
2021-12-08 10:13:05 -0300

    migration column 'tabuleiro'

---
+++ database/migrations/2021_12_07_001551_create_tabuleiro_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTestTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tabuleiro', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->integer('altura');
+            $table->integer('largura');
+            
+    
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tabuleiro');
+    }
+}
