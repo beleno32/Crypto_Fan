59a21a2303770fd806cdedfd61990ad0e3160693
José Luis <jl_beleno@id.uff.br>
2021-12-29 20:36:37 -0300

    Criação de Modelo e Seeders

--- app/Models/Action.php
+++ app/Models/Action.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Action extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'direction', 'steps_number', 'next_action', 'type'
+    ];
 }
