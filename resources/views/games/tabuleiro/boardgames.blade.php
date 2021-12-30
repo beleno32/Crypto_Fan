d18e683112851afc08d14918ae3894cfb78bd383
Gustavo <gu.viega1604@hotmail.com>
2021-12-30 17:03:42 -0300

    Incluindo id nas DIV's do tabuleiro.

--- resources/views/games/tabuleiro/boardgames.blade.php
+++ resources/views/games/tabuleiro/boardgames.blade.php
@@ -1,12 +1,12 @@
 <script src="style.css"></script>
   <div style="width:440px; height:440px ">
-        @for ($j = 0; $j<8; $j++)
-        @for ($i = 0; $i<8; $i++)
-        @if ($i % 2 == $j % 2)
-        <div style="background-color:black; width:{{440/8}}px; height:{{440/8}}px; display:inline-block; border:0; font-size:0; float:left;"></div>
-        @else
-        <div style="background-color:gray; width:{{440/8}}px; height:{{440/8}}px; display:inline-block; border:0; font-size:0; float:left;"></div>
-        @endif
-        @endfor
+        @for ($j = ord("A"); $j<ord("A")+8; $j++)
+            @for ($i = 1; $i<=8; $i++) 
+                  @if ($i % 2 == $j % 2)
+                        <div id="{{chr($j).$i}}" style="background-color:black; width:{{440/8}}px; height:{{440/8}}px; float:left;"></div>
+                  @else
+                        <div id="{{chr($j).$i}}" style="background-color:gray; width:{{440/8}}px; height:{{440/8}}px; float:left;"></div>
+                  @endif
+            @endfor
         @endfor
   </div>
